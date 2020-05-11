e7221a79ce18e690a01384eb57484efbea934265
ryanyogan <ryanyogan@gmail.com>
2020-05-10 17:22:15 -0700

    initial commit

---
+++ mix.exs
@@ -0,0 +1,46 @@
+defmodule SocketGallows.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :socket_gallows,
+      version: "0.1.0",
+      elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      mod: {SocketGallows.Application, []},
+      extra_applications: [:logger, :runtime_tools]
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  defp deps do
+    [
+      {:phoenix, "~> 1.5.0"},
+      {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:telemetry_metrics, "~> 0.4"},
+      {:telemetry_poller, "~> 0.4"},
+      {:gettext, "~> 0.11"},
+      {:jason, "~> 1.0"},
+      {:plug_cowboy, "~> 2.0"}
+    ]
+  end
+
+  defp aliases do
+    [
+      setup: ["deps.get", "cmd npm install --prefix assets"]
+    ]
+  end
+end
